5de1f15e0ffae2b860353e17c58b654916b10c57
Martin Tournoij <martin@arp242.net>
2017-03-19 07:16:30 +0000

    Fix tests for niet-unicode omgeving

--- download_npo/init_test.py
+++ download_npo/init_test.py
@@ -1,24 +1,32 @@
 # encoding:utf-8
 
+import locale
 import unittest
 import download_npo
 
 
 class Test_DownloadNPO(unittest.TestCase):
     def test_replace_vars(self):
-        self.assertEqual(download_npo.replace_vars('/hello/{titel}', {'titel': 'world'}),
-                         '/hello/world')
-        self.assertEqual(download_npo.replace_vars('/hello/{titel}', {'titel': u'ωορλδ'}),
-                         u'/hello/ωορλδ')
+        self.assertEqual(download_npo.replace_vars('/hello/a-{titel}', {'titel': 'world'}),
+                         '/hello/a-world')
+
+        if locale.getpreferredencoding() != 'UTF-8':
+            expected = u'/hello/a-'
+        else:
+            expected = u'/hello/a-ωορλδ'
+        self.assertEqual(download_npo.replace_vars('/hello/a-{titel}', {'titel': u'ωορλδ'}),
+                         expected)
 
     def test_make_filename(self):
         cases = (
-            (u'world', u'/tmp/world.mp4', {}),
-            (u'ωορλδ', u'/tmp/ωορλδ.mp4', {}),
-            (u'ωορλδ', u'/tmp/ωορλδ.mp4', {}),
+            (u'world', u'/tmp/a-world.mp4', u'/tmp/a-world.mp4', {}),
+            (u'ωορλδ', u'/tmp/a-ωορλδ.mp4', u'/tmp/a-.mp4', {}),
+            (u'ωορλδ', u'/tmp/a-ωορλδ.mp4', u'/tmp/a-.mp4', {}),
         )
-        for tc, expected, args in cases:
-            path = download_npo.make_filename('/tmp', '{titel}', 'mp4', {'titel': tc}, **args)
+        for tc, expected, expected2, args in cases:
+            path = download_npo.make_filename('/tmp', 'a-{titel}', 'mp4', {'titel': tc}, **args)
+            if locale.getpreferredencoding() != 'UTF-8':
+                expected = expected2
             self.assertEqual(path, expected)
 
 
@@ -30,4 +38,7 @@ class Test_Integration(unittest.TestCase):
         self.assertEqual(meta.get('STATUS'), 'OK')
 
         path = download_npo.make_filename('/tmp', '{titel}', 'mp4', meta)
-        self.assertEqual(path, u'/tmp/Gouden_Jaren_Indonesië.mp4')
+        if locale.getpreferredencoding() != 'UTF-8':
+            self.assertEqual(path, u'/tmp/Gouden_Jaren_Indonesie.mp4')
+        else:
+            self.assertEqual(path, u'/tmp/Gouden_Jaren_Indonesië.mp4')
