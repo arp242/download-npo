039fc091ab65b0ae3b57daee606c17e95f7683d7
Martin Tournoij <martin@arp242.net>
2017-03-19 06:14:08 +0000

    Nog wat style fixes

--- test.py
+++ test.py
@@ -3,18 +3,12 @@
 import doctest
 import unittest
 import download_npo
-
-#import coverage
-#cov = coverage.Coverage()
-#cov.start()
+import download_npo.sites
 
 doctest.testmod(download_npo)
+doctest.testmod(download_npo.sites)
 
 loader = unittest.TestLoader()
 test = loader.discover('.', pattern='*_test.py')
 runner = unittest.runner.TextTestRunner()
 runner.run(test)
-
-#cov.stop()
-#cov.save()
-#cov.report(omit=['*_test.py'])
